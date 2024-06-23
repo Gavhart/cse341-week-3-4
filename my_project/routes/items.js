3053f654fa18697eca8a4093501d72eb5a22da80
gavhart <har19076@byui.edu>
2024-06-22 20:17:59 -0600

    hmmm

--- my_project/routes/items.js
+++ my_project/routes/items.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Item = require("../../models/item");
+const Item = require("../models/item");
 
 // Middleware to get an item by ID
 async function getItem(req, res, next) {
